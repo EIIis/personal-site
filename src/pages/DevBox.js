8b74a58f226aa5b21fbd5886dbfe55a930bb30a6
Ellis <ellisalcantara@gmail.com>
2022-12-21 21:43:04 -0600

    Rearranged file structure

---
+++ src/pages/DevBox.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function DevBox() {
+    return (
+      <div className="App">
+        <p>Welcome to my dev box! I know you looked at my code to find this website haha nice. Anyways this is me working on my single page portfolio 
+            so i'm pretty happy you found it, so you can follow along! Let me know your thoughts!
+        </p>
+      </div> 
+    );
+  }
+  
+  export default DevBox;
+  
